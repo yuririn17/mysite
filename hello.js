1e854833e5e9ec5011079f80c4f3b4e69f641191
yuririn17 <snowman14abe@gmail.com>
2021-04-18 09:35:31 +0900

    1st commit

---
+++ hello.js
@@ -0,0 +1,13 @@
+console.log("Helloworld");
+
+let total = 0;
+//for(let i=0 i<10; i++){
+//    total += i;
+//    console.log(total);
+//}
+
+let i =0;
+while(i<10){
+    console.log(i);
+    i++;
+}
---
+++ humans.js
@@ -0,0 +1,25 @@
+
+const sato = {
+    // それぞれのプロパティは`,`で区切る
+    name: "佐藤さん",
+    height: 170,
+    weight: 60
+};
+
+const tanaka = {
+    // それぞれのプロパティは`,`で区切る
+    name: "田中さん",
+    height: 165,
+    weight: 55
+};
+
+const takahashi = {
+    // それぞれのプロパティは`,`で区切る
+    name: "高橋さん",
+    height: 180,
+    weight: 70
+};
+
+const humans = [sato, tanaka, takahashi];
+
+console.log(humans);
